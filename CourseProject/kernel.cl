52b2487475e80f81b252f46ae5e489aec68d89c0
Olleri01 <olli.lukkarinen@kotinet.com>
2025-04-11 16:20:54 +0300

    Multiprocess implementation (not ready)

--- CourseProject/kernel.cl
+++ CourseProject/kernel.cl
@@ -81,7 +81,106 @@ __kernel void FilterImage(__global unsigned char* image, __global unsigned char*
 
 }
 
-__kernel void ProcessImage(__global unsigned char* image, __global unsigned char* output, const unsigned width, const unsigned height) {
+double CalcMean(__global unsigned char* image, int x, int y, int width, int height, int windowSize, int d) {
+
+    int halfWindow = windowSize/2;
+    double result = 0.0;
+    int validPixels = 0;
+
+    for(int dy = -halfWindow; dy < halfWindow; dy++) {
+
+        for(int dx = -halfWindow; dx < halfWindow; dx++) {
+
+            if(y+dy < 0 || y+dy > height || x+dx-d < 0 || x+dx > width) continue;
+
+            int index = ((y+dy)*width+(x+dx-d))*4;
+            result += image[index];
+            validPixels++;   
+        }
+    }
+
+    if(validPixels != 0) {
+        result /= validPixels;
+    }
+    
+    return result;
+}
+
+
+double CalcZNCC(__global unsigned char * image1, __global unsigned char * image2, int x, int y, int width, int height, int windowSize, int d) {
+
+    int halfWindow = windowSize/2;
+    double result = 0;
+    int index1;
+    int index2;
+    double mean1;
+    double mean2;
+    double std1;
+    double std2;
+
+    for(int dy = -halfWindow; dy < halfWindow; dy++) {
+
+        for(int dx = -halfWindow; dx < halfWindow; dx++) {
+
+            if (y+dy >= height || x+dx >= width || x+dx-d < 0) continue;
+
+            index1 = ((y+dy)*width+(x+dx))*4;
+            index2 = ((y+dy)*width+(x+dx-d))*4;
+
+            mean1 = CalcMean(image1, x, y, width, height, windowSize, 0);
+            mean2 = CalcMean(image2, x, y, width, height, windowSize, d);
+
+            std1 += (image1[index1] - mean1) * (image1[index1] - mean1);
+            std2 += (image2[index2] - mean2) * (image2[index2] - mean2);
+            result += (image1[index1] - mean1) * (image2[index2] - mean2);
+        }
+    }
+
+    if(std1 != 0 && std2 != 0) {
+        result /= sqrt(std1)*sqrt(std2);
+    }
+
+    return result;
+}
+
+
+__kernel void ZNCCKernel(__global unsigned char* image1, __global unsigned char* image2, global unsigned char* outputImage, int width, int height, int windowSize, int max_disp) {
+
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+
+    double best_val;
+    double best_d;
+
+    double mean1 = CalcMean(image1, x, y, width, height, windowSize, 0);
+
+    for(int d = 0; d < max_disp; d++) {
+
+        double mean2 = CalcMean(image2, x, y, width, height, windowSize, d);
+
+        double zncc_val = CalcZNCC(image1, image2, x, y, width, height, windowSize, d);
+        
+        if(zncc_val > best_val) {
+            best_val = zncc_val;
+            best_d = d;
+        }
+
+    }
+
+    int index = (y*width+x)*4;
+
+    best_d = round((best_d/max_disp)*255.0);
+
+	outputImage[index] = best_d;
+	outputImage[index+1] = best_d;
+	outputImage[index+2] = best_d;
+	outputImage[index+3] = 255;
+
+}
+
+
+
+/*  __kernel void ProcessImage(__global unsigned char* image, __global unsigned char* output, const unsigned width, const unsigned height) {
 
     unsigned newWidth = width/4;
     unsigned newHeight = height/4;
@@ -92,4 +191,4 @@ __kernel void ProcessImage(__global unsigned char* image, __global unsigned char
 
     FilterImage(output, output, newWidth, newHeight);
 
-}
+} */
--- CourseProject/main.cpp
+++ CourseProject/main.cpp
@@ -6,6 +6,9 @@
 
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #include <CL/cl.h>
+#include <thread>
+#include <mutex>
+#include <algorithm>
 #include <cmath>
 #include <stdio.h>
 #include <chrono>
@@ -17,6 +20,7 @@
 #include <string>
 #include <sstream>
 #include "ImageData.hpp"
+#include <omp.h>
 #define MATRIXSIZE 100
 
 std::chrono::duration<double> opTime;
@@ -64,6 +68,7 @@ std::string loadKernelFromFile(std::string filename) {
 	}
 
 }
+
 
 std::vector<unsigned char> CalcZNCC(std::vector<unsigned char> image1, std::vector<unsigned char> image2, unsigned int imageWidth, unsigned int imageHeight, int windowWidth, int windowHeight, unsigned int max_disp) {
 
@@ -121,6 +126,8 @@ std::vector<unsigned char> CalcZNCC(std::vector<unsigned char> image1, std::vect
 					}
 				}
 
+				if(validPixels == 0) continue;
+
 				if(validPixels > 0) {
 					mean1 /= validPixels;
 					mean2 /= validPixels;
@@ -169,41 +176,248 @@ std::vector<unsigned char> CalcZNCC(std::vector<unsigned char> image1, std::vect
 		}
 	}
 
+	std::cout << "Calculated ZNCC" << std::endl;
+
 	return output;
 
 }
 
 
+std::vector<unsigned char> CalcZNCC_multithreaded(
+    const std::vector<unsigned char>& image1,
+    const std::vector<unsigned char>& image2,
+    unsigned int width, unsigned int height,
+    int win_size, unsigned int max_disp,
+    unsigned int num_threads = std::thread::hardware_concurrency())
+{
+    std::vector<unsigned char> output(image1.size());
+    const int half_win = win_size / 2;
+    std::vector<std::thread> threads;
+    std::mutex mtx; // Only needed if writing to shared resources
+
+    // Calculate rows per thread
+    const int rows_per_thread = (height + num_threads - 1) / num_threads;
+
+    auto worker = [&](int thread_id) {
+        const int start_row = thread_id * rows_per_thread;
+        const int end_row = std::min<int>(start_row + rows_per_thread, height);
+
+        for (int j = start_row; j < end_row; j++) {
+            for (int i = 0; i < width; i++) {
+                float best_zncc = -1.0f;
+                int best_d = max_disp;
+
+                const int y_start = std::max(0, j - half_win);
+                const int y_end = std::min<int>(height - 1, j + half_win);
+
+                for (int d = 0; d < max_disp; d++) {
+                    const int x_start = std::max(0, i - half_win);
+                    const int x_end = std::min<int>(width - 1, i + half_win);
+                    const int x_start_right = std::max(0, x_start - d);
+
+                    if (x_start_right > x_end) continue;
+
+                    // First pass: means
+                    float mean1 = 0.0f, mean2 = 0.0f;
+                    int valid_pixels = 0;
+
+                    for (int wy = y_start; wy <= y_end; wy++) {
+                        const size_t row_offset = wy * width * 4;
+                        for (int wx = x_start; wx <= x_end; wx++) {
+                            if (wx - d < 0) continue;
+
+                            mean1 += image1[row_offset + wx * 4];
+                            mean2 += image2[row_offset + (wx - d) * 4];
+                            valid_pixels++;
+                        }
+                    }
+
+                    if (valid_pixels == 0) continue;
+                    mean1 /= valid_pixels;
+                    mean2 /= valid_pixels;
+
+                    // Second pass: ZNCC components
+                    float std1 = 0.0f, std2 = 0.0f, covar = 0.0f;
+
+                    for (int wy = y_start; wy <= y_end; wy++) {
+                        const size_t row_offset = wy * width * 4;
+                        for (int wx = x_start; wx <= x_end; wx++) {
+                            if (wx - d < 0) continue;
+
+                            const float val1 = image1[row_offset + wx * 4] - mean1;
+                            const float val2 = image2[row_offset + (wx - d) * 4] - mean2;
+
+                            std1 += val1 * val1;
+                            std2 += val2 * val2;
+                            covar += val1 * val2;
+                        }
+                    }
+
+                    // Compute ZNCC
+                    if (std1 > 0 && std2 > 0) {
+                        const float zncc = covar / (sqrtf(std1) * sqrtf(std2));
+                        if (zncc > best_zncc) {
+                            best_zncc = zncc;
+                            best_d = d;
+                            if (best_zncc > 0.95f) break;
+                        }
+                    }
+                }
+
+                // Write output (no mutex needed as each thread writes to different rows)
+                const size_t idx = (j * width + i) * 4;
+                const unsigned char disp_val = static_cast<unsigned char>((best_d * 255) / max_disp);
+                output[idx] = output[idx + 1] = output[idx + 2] = disp_val;
+                output[idx + 3] = 255;
+            }
+        }
+        };
+
+    // Launch threads
+    for (unsigned t = 0; t < num_threads; t++) {
+        threads.emplace_back(worker, t);
+    }
+
+    // Join threads
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    return output;
+}
+
+std::vector<unsigned char> OcculsionFill(std::vector<unsigned char> map, unsigned int width, unsigned int height) {
+
+	int index;
+	int index2;
+	std::vector<unsigned char> outputMap = map;
+	bool flag;
+
+	for(int y = 0; y < height; y++) {
+		for(int x = 0; x < width; x++) {
+
+			index = (y*width+x)*4;
+			
+			if(map[index] == 0) {
+
+				flag = false;
+
+				for(int n = 1; n < 10 && !flag; n++) {
+
+					for(int i = -n; i <= n && !flag; i++) {
+
+						for(int j = -n; j <= n; j++) {
+	
+							index2 = ((y+i)*width+(x+j))*4;
+	
+							if(index2 > map.size() || index2 < 0 || map[index2] == 0) {
+								continue;
+							} else {
+								outputMap[index] = map[index2];
+								outputMap[index+1] = map[index2+1];
+								outputMap[index+2] = map[index2+2];
+								flag = true;
+								break;
+							}
+						}
+					}
+				}
+
+			}
+
+		}
+	}
+
+	std::cout << "Occulsion fill complete" << std::endl;
+
+	return outputMap;
+}
+
+
+
+
+
 std::vector<unsigned char> CrossCheck(std::vector<unsigned char> map1, std::vector<unsigned char> map2, unsigned int threshold) {
 
-	std::vector<unsigned char> outputMap;
-	outputMap.resize(map1.size());
+	std::vector<unsigned char> outputMap = map1;
 
+	#pragma omp parallel for
 	for(int i = 0; i < map1.size(); i++) {
 
 		if(std::abs(map1[i] - map2[i]) > threshold) {
 			outputMap[i] = 0;
-		} else  {
-			outputMap[i] = map1[i];
 		}
 	}
+
+	std::cout << "Cross check complete" << std::endl;
  
 	return outputMap;
 }
 
 
-/* std::vector<unsigned char> OcculsionFill(std::vector<unsigned char> map) {
+std::vector<unsigned char> OcclusionFillMultithreaded(std::vector<unsigned char> map, unsigned int width, unsigned int height) {
 
-	for(int i = 0; i < map.size(); i++) {
+    unsigned int num_threads = omp_get_max_threads();
+    std::vector<std::vector<unsigned char>> threadOutputs(num_threads, map);
 
-		if(map[i] == 0){
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        auto& localOutput = threadOutputs[thread_id];
 
-		} 
-	}
+        #pragma omp for collapse(2)
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
 
+                int index = (y * width + x) * 4;
+
+                if (map[index] == 0) {
+
+                    bool flag = false;
+
+                    for (int n = 1; n < 10 && !flag; n++) {
+                        for (int i = -n; i <= n && !flag; i++) {
+                            for (int j = -n; j <= n; j++) {
+
+								int index2 = ((y+i)*width+(x+j))*4;
+
+								if(index2 > map.size() || index2 < 0 || map[index2] == 0) {
+									continue;
+								} else {
+									localOutput[index] = map[index2];
+									localOutput[index+1] = map[index2+1];
+									localOutput[index+2] = map[index2+2];
+									flag = true;
+									break;
+								}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    std::vector<unsigned char> outputMap = map;
+
+    for (size_t i = 0; i < map.size(); i++) {
+        if (map[i] == 0) {
+            for (unsigned int t = 0; t < num_threads; ++t) {
+                if (threadOutputs[t][i] != 0) {
+                    outputMap[i] = threadOutputs[t][i];
+                    break;
+                }
+            }
+        }
+    }
+
+    std::cout << "Occlusion fill complete using OpenMP" << std::endl;
+    return outputMap;
 }
- */
-int main() {
+
+
+
+/* int main() {
 
 	ImageData image1("im0.png");
 	ImageData image2("im1.png");
@@ -214,21 +428,23 @@ int main() {
 	image1.ImageToGrayscale();
 	image2.ImageToGrayscale();
 
-	std::vector<unsigned char> map1 = CalcZNCC(image1.getImage(), image2.getImage(), image1.getWidth(), image1.getHeight(), 9, 9, 260);
-	//std::vector<unsigned char> map2 = CalcZNCC(image2.getImage(), image1.getImage(), image1.getWidth(), image1.getHeight(), 9, 9, 260);
+	std::vector<unsigned char> map1 = CalcZNCC_multithreaded(image1.getImage(), image2.getImage(), image1.getWidth(), image1.getHeight(), 9, 260);
+	std::vector<unsigned char> map2 = CalcZNCC_multithreaded(image2.getImage(), image1.getImage(), image1.getWidth(), image1.getHeight(), 9, 260);
 
-	//std::vector<unsigned char> mapOut = CrossCheck(map1, map2, 8);
+	std::vector<unsigned char> mapX = CrossCheck(map1, map2, 8);
 
-	ImageData outImage(map1, image1.getWidth(), image1.getHeight());
+	std::vector<unsigned char> mapOut = OcclusionFillMultithreaded(mapX, image1.getWidth(), image1.getHeight());
+
+	ImageData outImage(mapOut, image1.getWidth(), image1.getHeight());
+
 	outImage.WriteImageToFile("output.png");
 
 	return 0;
-}
+} */
 
 
-/*
-int main() {
 
+/* int main() {
 
 	auto start = std::chrono::high_resolution_clock::now();
 
@@ -301,5 +517,4 @@ int main() {
 	std::cout << "Execution time: " << opTime.count() << "s" << std::endl;
 
     return 0;
-}
-*/
+} */
